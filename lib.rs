852d062e44c19f9f285a7e2ab58d34e91a7f1e38
navigaid <navigaid@gmail.com>
2022-11-13 22:51:27 +0800

    init contract

---
+++ lib.rs
@@ -0,0 +1,20 @@
+#[no_mangle]
+pub extern "C" fn init() {}
+
+#[no_mangle]
+pub extern "C" fn handle() {}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
