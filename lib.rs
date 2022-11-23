7bb01d632e815271ae4beb63ab36df3562e52870
navigaid <navigaid@gmail.com>
2022-11-23 14:56:50 +0800

    add fn set_pixel && toggle_pixel && dump_all

--- lib.rs
+++ lib.rs
@@ -4,17 +4,26 @@
 use gstd::prelude::*;
 
 mod contract {
-    use crate::*;
-
-    #[derive(Default)]
-    pub struct Contract(pub bool);
+    pub struct Contract(pub [bool; 401]);
 
     impl Contract {
+        pub fn dump_all(&self) -> [bool; 401] {
+            self.0
+        }
+        pub fn pixel(&self, n: u16) -> bool {
+            self.0[n as usize]
+        }
         pub fn set(&mut self, b: bool) {
-            self.0 = b
+            self.set_pixel(0, b)
         }
         pub fn toggle(&mut self) {
-            self.set(!self.0)
+            self.set(!self.pixel(0))
+        }
+        pub fn set_pixel(&mut self, n: u16, b: bool) {
+            self.0[n as usize] = b
+        }
+        pub fn toggle_pixel(&mut self, n: u16) {
+            self.set_pixel(n, !self.pixel(n))
         }
     }
 }
@@ -31,11 +40,13 @@ mod codec {
         pub enum Action {
             Toggle,
             Set(bool),
+            TogglePixel(u16),
+            SetPixel(u16, bool),
         }
 
         #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
         pub enum Event {
-            SetTo(bool),
+            SetTo(u16, bool),
         }
     }
 
@@ -44,11 +55,13 @@ mod codec {
         #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
         pub enum Query {
             Status,
+            DumpAll,
         }
 
         #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
         pub enum State {
             StatusOf(bool),
+            All([bool; 401]),
         }
     }
 }
@@ -63,7 +76,7 @@ static mut STATE: Option<Contract> = None;
 // what this line says is "here is a C function written in Rust"
 #[no_mangle]
 unsafe extern "C" fn init() {
-    STATE = Some(Contract::default());
+    STATE = Some(Contract([false; 401]));
 }
 
 #[no_mangle]
@@ -73,11 +86,19 @@ unsafe extern "C" fn handle() {
     match action {
         Action::Set(b) => {
             state.set(b);
-            gstd::msg::reply(Event::SetTo(state.0), 0).expect("failed to reply");
+            gstd::msg::reply(Event::SetTo(0, state.pixel(0)), 0).expect("failed to reply");
         }
         Action::Toggle => {
             state.toggle();
-            gstd::msg::reply(Event::SetTo(state.0), 0).expect("failed to reply");
+            gstd::msg::reply(Event::SetTo(0, state.pixel(0)), 0).expect("failed to reply");
+        }
+        Action::SetPixel(n, b) => {
+            state.set_pixel(n, b);
+            gstd::msg::reply(Event::SetTo(n, state.pixel(n)), 0).expect("failed to reply");
+        }
+        Action::TogglePixel(n) => {
+            state.toggle_pixel(n);
+            gstd::msg::reply(Event::SetTo(n, state.pixel(n)), 0).expect("failed to reply");
         }
     }
 }
@@ -87,7 +108,8 @@ unsafe extern "C" fn meta_state() -> *mut [i32; 2] {
     let state = STATE.as_ref().expect("failed to get contract state");
     let query: Query = gstd::msg::load().expect("failed to load query");
     let result = match query {
-        Query::Status => State::StatusOf(state.0),
+        Query::Status => State::StatusOf(state.pixel(0)),
+        Query::DumpAll => State::All(state.dump_all()),
     };
     gstd::util::to_leak_ptr(result.encode())
 }
