a72a6cab4c733bcf7873909b2f384fd813d7be14
navigaid <navigaid@gmail.com>
2022-11-30 14:36:19 +0800

    init groupchat

--- lib.rs
+++ lib.rs
@@ -3,25 +3,6 @@
 
 use gstd::prelude::*;
 
-mod contract {
-    use crate::*;
-
-    #[derive(Default)]
-    pub struct Contract(BTreeMap<String, String>);
-
-    impl Contract {
-        pub fn add_url(&mut self, code: String, url: String) {
-            self.0
-                .try_insert(code, url)
-                .expect("failed to add url: code exists");
-        }
-        pub fn get_url(&self, code: String) -> Option<String> {
-            self.0.get(&code).cloned()
-        }
-    }
-}
-use contract::Contract;
-
 mod codec {
     use crate::*;
     use parity_scale_codec::{Decode, Encode};
@@ -31,70 +12,31 @@ mod codec {
         use super::*;
         #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
         pub enum Action {
-            AddUrl { code: String, url: String },
+            AddMsg(String),
         }
 
         #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
         pub enum Event {
-            Added { code: String, url: String },
-        }
-    }
-
-    pub mod query {
-        use super::*;
-        #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
-        pub enum Query {
-            Code(String),
-        }
-
-        #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
-        pub enum State {
-            MaybeUrl(Option<String>),
+            AddedMsg { msg: String, by: gstd::ActorId },
         }
     }
 }
-use codec::{
-    action::{Action, Event},
-    query::{Query, State},
-};
-
-// contract state
-static mut STATE: Option<Contract> = None;
-
-// what this line says is "here is a C function written in Rust"
-#[no_mangle]
-unsafe extern "C" fn init() {
-    STATE = Some(Contract::default());
-}
+use codec::action::{Action, Event};
 
 #[no_mangle]
-unsafe extern "C" fn handle() {
-    let state = STATE.as_mut().expect("failed to get state as mut");
+extern "C" fn handle() {
     let action: Action = gstd::msg::load().expect("failed to load action");
     match action {
-        Action::AddUrl { code, url } => {
-            state.add_url(code.clone(), url.clone());
-            gstd::msg::reply(Event::Added { code, url }, 0).expect("failed to reply");
+        Action::AddMsg(msg) => {
+            let by = gstd::exec::origin();
+            gstd::msg::reply(Event::AddedMsg { msg, by }, 0).expect("failed to reply");
         }
     }
-}
-
-#[no_mangle]
-unsafe extern "C" fn meta_state() -> *mut [i32; 2] {
-    let state = STATE.as_ref().expect("failed to get contract state");
-    let query: Query = gstd::msg::load().expect("failed to load query");
-    let result = match query {
-        Query::Code(code) => State::MaybeUrl(state.get_url(code)),
-    };
-    gstd::util::to_leak_ptr(result.encode())
 }
 
 gstd::metadata! {
-    title: "github.com/btwiuse/gurls",
+    title: "groupchat",
     handle:
         input: Action,
         output: Event,
-    state:
-        input: Query,
-        output: State,
 }
