4a08c565fd446ab30a35d8cdb054ee055f7bcc40
navigaid <navigaid@gmail.com>
2023-03-19 18:32:56 +0800

    bump deps

--- script/meta.ts
+++ script/meta.ts
@@ -2,11 +2,11 @@ import { getProgramMetadata } from "https://github.com/btwiuse/gear-js/raw/deno/
 import {
   hexToU8a,
   u8aToHex,
-} from "https://deno.land/x/polkadot@0.2.31/util/index.ts";
+} from "https://deno.land/x/polkadot@0.2.32/util/index.ts";
 import {
   blake2AsHex,
   blake2AsU8a,
-} from "https://deno.land/x/polkadot@0.2.31/util-crypto/index.ts";
+} from "https://deno.land/x/polkadot@0.2.32/util-crypto/index.ts";
 
 export function metaTxtU8a(): Uint8Array {
   return hexToU8a(Deno.readTextFileSync("meta.txt"));
